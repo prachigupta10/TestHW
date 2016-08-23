1c3f128268eca08ac3221ce5fa9242f528b83355
PrachiGupta <prachi_gupta10@infosys.com>
2016-08-23 21:17:53 +0530

    committing first version, 9:16

---
+++ TestHW/src/com/eta/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.eta.test;
+
+public class HelloWorld {
+	
+	public static void displayGreeting(){
+		System.out.println("hello world");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Printing Greeting");
+		HelloWorld.displayGreeting();
+	}
+
+}
